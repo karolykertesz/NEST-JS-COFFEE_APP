5e167869df4bb5875c3573b77a2f2f2a7fbcf09e
karolykertesz <kforcegroup@hotmail.com>
2021-09-19 20:36:15 +0200

    k

--- src/coffee/coffee.entity.ts
+++ src/coffee/coffee.entity.ts
@@ -1,18 +1,22 @@
-
 import { type } from "os";
-import { Column, Entity, JoinTable, ManyToMany, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn
+} from "typeorm";
 import { Flavour } from "./flavour.entity";
 
 @Entity()
 export class Coffee {
   @PrimaryGeneratedColumn()
-  id:number;
+  id: number;
   @Column()
   name: string;
-@JoinTable()
-@ManyToMany(type => Flavour,(flavor)=> flavor.coffees,{cascade:true})
+  @JoinTable()
+  @ManyToMany((type) => Flavour, (flavor) => flavor.coffees, { cascade: true })
   flavours: string[];
   @Column()
   location: string;
-
 }
